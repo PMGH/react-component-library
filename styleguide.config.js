69c499d5c2e9315f6d81ef1b033f7e6c9e8ce6df
Peter McCready <petermccready2@gmail.com>
2020-02-09 17:28:44 +0000

    remove typescript reactdoc parser

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   webpackConfig: require('./webpack.config.js'),
   components: 'src/components/**/*.jsx',
-  propsParser: require('react-docgen-typescript').withCustomConfig(
-    './tsconfig.json'
-  ).parse,
   require: [
     path.join(__dirname, './src/index.scss')
   ]
