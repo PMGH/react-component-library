38515840ba520fddd635341c3cb02b557f86b8fa
Peter McCready <petermccready2@gmail.com>
2020-02-12 19:54:16 +0000

    add IconBar component and update scss
    includes gitignore /styleguide

---
+++ src/components/IconBar/IconBar.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+* An icon bar component
+*/
+export default class IconBar extends React.Component {
+  static propTypes = {
+    /** REQUIRED: The iconUrl prop is the source URL of the icon. */
+    iconUrl: PropTypes.string.isRequired,
+    /** REQUIRED: The text prop represents the text that will be displayed. */
+    text: PropTypes.string.isRequired
+  }
+
+  render() {
+    return (
+      <div className="icon-bar">
+        <img src={this.props.iconUrl} alt="Icon" className="icon" />
+        <span>{this.props.text}</span>
+      </div>
+    );
+  }
+}
---
+++ src/components/IconBar/IconBar.md
@@ -0,0 +1,8 @@
+Example
+
+```js padded
+<IconBar
+  iconUrl="https://cdn1.iconfinder.com/data/icons/cryptocurrency-blockchain-fintech-free/32/Cryptocurrency_Bitcoin_BTC-512.png"
+  text="Bitcoin!"
+/>
+```
