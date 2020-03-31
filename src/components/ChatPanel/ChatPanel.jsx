978cc28ac9d58bd174a16790ff0c8301f47b65fa
Peter McCready <petermccready2@gmail.com>
2020-03-31 14:30:57 +0100

    add basic messaging with timings

--- src/components/ChatPanel/ChatPanel.jsx
+++ src/components/ChatPanel/ChatPanel.jsx
@@ -5,25 +5,58 @@ import Panel from '../Panel/Panel';
 * A chat panel component that renders it's children.
 */
 export default class ChatPanel extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
   handleKeyDown(event) {
     if (event.key === 'Enter') {
       const message = event.target.value;
       if (message.length > 0) {
-        console.log('message: ', message)
         // submit message
+        this.setState({
+          messages: [...this.state.messages, message]
+        })
+        this.clearInput();
       }
     }
   }
 
   renderMessages() {
     return(
-      <p>Messages from websocket</p>
+      this.state.messages.map((message, index) => (
+        <div className="chat-message">
+          <p key={index} className="chat-message-content">{message}</p>
+          <span className="chat-time">{this.time()}</span>
+        </div>
+    ))
     )
+  }
+
+  time() {
+    const now = new Date();
+    return now.toUTCString();
+  }
+
+  clearInput() {
+    const chatInputs = document.getElementsByClassName("chat-input");
+    chatInputs.forEach(input => input.value = '');
+  }
+
+  scrollToBottom() {
+    const chatOutputs = document.getElementsByClassName("chat-output");
+    chatOutputs.forEach(output => output.scrollTop = output.scrollHeight)
   }
 
   render() {
     return (
-      <Panel>
         <div className="chat-container">
           <div className="chat-output">
             { this.renderMessages() }
@@ -38,7 +71,6 @@ export default class ChatPanel extends React.Component {
             </input>
           </div>
         </div>
-      </Panel>
     );
   }
 }
