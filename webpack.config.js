8e2702aae6d4b5c25da471b761856c64bde14bcb
Peter McCready <petermccready2@gmail.com>
2020-10-23 21:22:24 +0100

    fix webpack svg load and deps

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'styleguide.bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "styleguide.bundle.js",
   },
   module: {
     rules: [
@@ -11,24 +11,38 @@ module.exports = {
         test: /\.scss$/,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "svg-url-loader",
+            options: {
+              limit: 10000,
+            },
+          },
         ],
       },
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-class-properties']
-          }
-        }
-      }
-    ]
-  }
-}
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
